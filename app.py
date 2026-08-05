c7572b44212a719957fa8e8f5c258861b3624286
Anindo Banerjee <31092883+Anindo94@users.noreply.github.com>
2026-08-06 02:54:59 +0530

    Update app.py
    fixing issue

--- app.py
+++ app.py
@@ -1,38 +1,6 @@
-from flask import Response
 from app import create_app
 
 app = create_app()
-
-@app.route('/sitemap.xml')
-def sitemap():
-    xml = '''<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url>
-    <loc>https://www.techguardianinfotech.com/</loc>
-    <priority>1.0</priority>
-  </url>
-  <url>
-    <loc>https://www.techguardianinfotech.com/services</loc>
-    <priority>0.9</priority>
-  </url>
-  <url>
-    <loc>https://www.techguardianinfotech.com/website-development</loc>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>https://www.techguardianinfotech.com/custom-software</loc>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>https://www.techguardianinfotech.com/cloud-solutions</loc>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>https://www.techguardianinfotech.com/digital-security</loc>
-    <priority>0.8</priority>
-  </url>
-</urlset>'''
-    return Response(xml, mimetype='application/xml')
 
 if __name__ == "__main__":
     print("Starting TechGuardian Infotech server...")
