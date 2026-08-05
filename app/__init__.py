562c022d33d1963b1eabf524d0075f3ec0db9561
Anindo Banerjee <31092883+Anindo94@users.noreply.github.com>
2026-08-06 02:53:02 +0530

    Update __init__.py
    Add sitemap.xml route in app factory

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, Response
 from .config import Config
 import os
 
@@ -15,5 +15,36 @@ def create_app():
 
     from .routes import main
     app.register_blueprint(main)
+
+    @app.route('/sitemap.xml')
+    def sitemap():
+        xml = '''<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://www.techguardianinfotech.com/</loc>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://www.techguardianinfotech.com/services</loc>
+    <priority>0.9</priority>
+  </url>
+  <url>
+    <loc>https://www.techguardianinfotech.com/website-development</loc>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://www.techguardianinfotech.com/custom-software</loc>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://www.techguardianinfotech.com/cloud-solutions</loc>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://www.techguardianinfotech.com/digital-security</loc>
+    <priority>0.8</priority>
+  </url>
+</urlset>'''
+        return Response(xml, mimetype='application/xml')
 
     return app
